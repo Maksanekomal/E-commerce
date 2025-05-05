736c2473415e408426c48b29727ee7172405fccd
Komal Maksane <komal20@gmail.com>
2025-05-05 17:42:47 +0530

    km

--- E-commerce/Ecommerce/src/Pages/AdminHomePage.jsx
+++ E-commerce/Ecommerce/src/Pages/AdminHomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const AdminHomePage = () => {
     const orders = [
@@ -10,7 +11,44 @@ const AdminHomePage = () => {
             },
             totalPrice: 110,
             status: "Processing",
-        }
+        },
+        {
+            _id: 123123,
+            user: {
+                name:"John Doe",
+
+            },
+            totalPrice: 110,
+            status: "Processing",
+        },
+        {
+            _id: 123123,
+            user: {
+                name:"John Doe",
+
+            },
+            totalPrice: 110,
+            status: "Processing",
+        },
+        {
+            _id: 123123,
+            user: {
+                name:"John Doe",
+
+            },
+            totalPrice: 110,
+            status: "Processing",
+        },
+        {
+            _id: 123123,
+            user: {
+                name:"John Doe",
+
+            },
+            totalPrice: 110,
+            status: "Processing",
+        },
+        
 
     ]
   return (
@@ -22,12 +60,51 @@ const AdminHomePage = () => {
                 <p className="text-2xl">$1000</p>
             </div>
             <div className="p-4 shadow-md rounded-lg">
-                <h2 className="text-xl font-semibold">Revenue</h2>
-                <p className="text-2xl">$1000</p>
+                <h2 className="text-xl font-semibold">Total Orders</h2>
+                <p className="text-2xl">200</p>
+                <Link to="/admin/orders" className="text-blue-500 hover:underline">
+                Manage Orders</Link>
             </div>
             <div className="p-4 shadow-md rounded-lg">
-                <h2 className="text-xl font-semibold">Revenue</h2>
-                <p className="text-2xl">$1000</p>
+                <h2 className="text-xl font-semibold">Total Products</h2>
+                <p className="text-2xl">100</p>
+                <Link to="/admin/products" className="text-blue-500 hover:underline">
+                Manage Products
+                </Link>
+            </div>
+        </div>
+        <div className="mt-6">
+            <h2 className="text-2xl font-bold mb-4">Recent Orders</h2>
+            <div className="overflow-x-auto">
+                <table className="min-w-full text-left text-gray-500">
+                    <thead className="bg-gray-100 text-xs uppercase text-gray-700">
+                        <tr>
+                            <th className="py-3 px-4">Order ID</th>
+                            <th className="py-3 px-4">User</th>
+                            <th className="py-3 px-4">Total Price</th>
+                            <th className="py-3 px-4">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders.length > 0 ? (
+                            orders.map((order) => (
+                                <tr key={order._id} className="border-b hover:bg-gray-50 currsor-pointer">
+                                    <td className="p-4">{order._id}</td>
+                                    <td className="p-4">{order.user.name}</td>
+                                    <td className="p-4">{order.totalPrice}</td>
+                                    <td className="p-4">{order.status}</td>
+                                </tr>
+
+                            ))
+                        ) : (
+                            <tr>
+                                <td colSpan={4} className="p-4 text-center text-gray-500">
+                                    No recent orders found.
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
             </div>
         </div>
       
